1e9e69902f2973cbdf1b7ee5501fc8289d202829
mzriela <mariela.austria@dawsoncollege.qc.ca>
2026-05-12 19:56:12 -0400

    Create Neon_Game.ino
    little start

---
+++ Neon_Game/Neon_Game.ino
@@ -0,0 +1,12 @@
+
+#include <LiquidCrystal.h> // Include the LiquidCrystal library for the LCD
+
+// Define pins connected to the pushbutton
+const int buttonPin = A0;
+
+// Define the LCD pin connections: RS, Enable, Data4, Data5, Data6, Data7
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12
+
+
+
+
